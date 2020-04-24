376871af7ebe00c5dd02207f72239b4eae4e42ff
Neyjund <robindutilloy@gmail.com>
2020-04-24 20:37:59 +0200

    modif

--- validation.js
+++ validation.js
@@ -1,1 +1,26 @@
+document.forms["connexion"].addEventListener("submit", function(e) {
+ 
+	var erreur;
+ 
+	var inputs = this;
+ 
+	for (var i = 0; i < inputs.length; i++) {
+		console.log(inputs[i]);
+		if (!inputs[i].value) {
+			erreur = "Veuillez renseigner tous les champs";
+			break;
+		}
+	}
+ 
+	if (erreur) {
+		e.preventDefault();
+		document.getElementById("erreur").innerHTML = erreur;
+		return false;
+	} else {
+		alert('Formulaire envoyé !');
+	}
+	
+});
+
+    
 
